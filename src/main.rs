1a053c5aea4f3fe6e133f487552bb2867ed1102c
Aliakbar <34204311+Salehbigdeli@users.noreply.github.com>
2026-04-25 04:11:53 +0200

    accurate rate limiter

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,38 @@
 use std::{
-    sync::{
-        Arc,
-        atomic::{AtomicU64, Ordering},
-    },
+    collections::VecDeque,
+    sync::{Arc, Mutex},
     thread,
-    time::{self, Duration},
+    time::{self, Duration, Instant},
 };
 
-struct RateLimiterState {
-    duration: Duration,
-    max_requests: u64,
-    requests_count: Arc<AtomicU64>,
-}
-
-impl RateLimiterState {
-    fn start(&self) {
-        let duration = self.duration;
-        let requests_count = Arc::clone(&self.requests_count);
-        thread::spawn(move || {
-            loop {
-                thread::sleep(duration);
-                requests_count.store(0, Ordering::Relaxed);
-            }
-        });
-    }
-}
 struct RateLimiter {
-    state: RateLimiterState,
+    duration: Duration,
+    requests: Arc<Mutex<VecDeque<Instant>>>,
+    max_request: usize,
 }
 
 impl RateLimiter {
-    fn new(max_reqs: u64, duration: Duration) -> Self {
-        let state = RateLimiterState {
+    fn new(max_reqs: usize, duration: Duration) -> Self {
+        Self {
             duration,
-            max_requests: max_reqs,
-            requests_count: Arc::new(AtomicU64::new(0)),
-        };
-        state.start();
-        Self { state }
+            requests: Arc::new(Mutex::new(VecDeque::new())),
+            max_request: max_reqs,
+        }
     }
     fn is_available(&mut self) -> bool {
-        if self.state.requests_count.load(Ordering::Relaxed) < self.state.max_requests {
-            self.state.requests_count.fetch_add(1, Ordering::Relaxed);
+        let now = Instant::now();
+        let mut requests = self.requests.lock().unwrap();
+
+        while let Some(&oldest) = requests.front() {
+            if now.duration_since(oldest) >= self.duration {
+                requests.pop_front();
+            } else {
+                break;
+            }
+        }
+
+        if requests.len() < self.max_request {
+            requests.push_back(now);
             true
         } else {
             false
@@ -50,47 +41,32 @@ impl RateLimiter {
 }
 
 fn main() {
-    // usage pattern 1:
-    println!("First scenario");
-    let started = time::Instant::now();
-    let mut rl = RateLimiter::new(10, Duration::from_secs(10));
-    for _ in 0..12 {
-        println!(
-            "    is service available at {:?}? {}",
-            time::Instant::now() - started,
-            rl.is_available()
-        );
-    }
-    thread::sleep(Duration::from_secs(10));
-    for _ in 0..5 {
-        println!(
-            "    is service available at {:?}? {}",
-            time::Instant::now() - started,
-            rl.is_available()
-        );
-    }
-    // end of pattern 1
-
     // usage pattern 2:
     // our rate limiter is not good for this pattern. It allows 20 requests in almost 2 seconds!
     println!("Second scenario");
     let started = time::Instant::now();
     let mut rl = RateLimiter::new(10, Duration::from_secs(10));
     thread::sleep(Duration::from_secs(9));
+    let mut r = 0;
     for _ in 0..10 {
+        r += 1;
         println!(
-            "    is service available at {:?}? {}",
+            "  {}  is service available at {:?}? {}",
+            r,
             time::Instant::now() - started,
             rl.is_available()
         );
     }
     thread::sleep(Duration::from_secs(2));
     for _ in 0..10 {
+        r += 1;
         println!(
-            "    is service available at {:?}? {}",
+            "  {}  is service available at {:?}? {}",
+            r,
             time::Instant::now() - started,
             rl.is_available()
         );
+        thread::sleep(Duration::from_secs(2));
     }
     // end of pattern 2
 }
